41fe46c4cb1fc7c60e48b5fcb3b180398200c740
Jorge Stefano <118781535+jorgestefanodev@users.noreply.github.com>
2023-07-14 20:40:30 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,8 +2,9 @@
 function login() {
     let usuario = document.getElementById('user').value
     let senha = document.getElementById('password').value
-    if (usuario == 'stefano' && senha == 123456){
-        alert('Login autorizado')        
+    if (usuario == 'stefano' && senha == 1234){
+        //alert('Login autorizado')  
+        window.location.href = "index.html"
     } else if (usuario == '' || senha == ''){
         alert('Preencha os dados corretamente')
     } else {
